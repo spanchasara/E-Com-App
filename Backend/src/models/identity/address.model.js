3ef992b567c12496b084654b9633bae7ac5a17c4
Lokesh Dhingra <lokesh2001dhingra@gmail.com>
2023-10-10 10:36:44 +0530

    Added Modals for user management

---
+++ Backend/src/models/identity/address.model.js
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from "mongoose";
+
+const addressSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    phoneNo: {
+      type: string,
+      trim: true,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    addressLane1: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    addressLane2: {
+      type: String,
+      trim: true,
+    },
+    landmark: {
+      type: String,
+      trim: true,
+    },
+    pincode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Address = mongoose.model("Address", addressSchema);
+
+export default Address;
