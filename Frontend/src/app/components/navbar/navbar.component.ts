7972ffa65f06e5f93dcddc14f31c72f25e3f32c6
Lokesh Dhingra <lokesh2001dhingra@gmail.com>
2023-10-13 09:40:20 +0530

    welcome page setup

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false;
 
+  getLoginStatus() {
+    return this.isLoggedIn;
+  }
+  login() {
+    this.isLoggedIn = true;
+  }
+  logout() {
+    this.isLoggedIn = false;
+  }
 }
